28aed8553fec5afcffc3a5471bc627f238ccf667
Maximilian Huber <external.maximilian.huber2@bosch-si.com>
2019-01-04 08:33:51 +0100

    minor things
    Signed-off-by: Maximilian Huber <external.maximilian.huber2@bosch-si.com>

--- init.sh
+++ init.sh
@@ -8,5 +8,7 @@ AUTO_START_SWARM=true vagrant up
 docker-machine rm vagrant-swarm-host
 docker-machine create --driver generic --generic-ip-address=192.168.10.2 --generic-ssh-key "swarm_id_rsa" --generic-ssh-user=vagrant vagrant-swarm-host
 docker-machine env vagrant-swarm-host
+docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
-echo "call:\n     eval \$(docker-machine env vagrant-swarm-host)"
+echo "call:"
+echo "     eval \$(docker-machine env vagrant-swarm-host)"
